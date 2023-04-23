3c66a764dbc77ce23d81d115ec87976e17b61feb
Andrey <kovorot0875@mail.ru>
2023-04-23 19:33:24 +0300

    addToCart method added

--- src/Basket.java
+++ src/Basket.java
@@ -1,9 +1,15 @@
 public class Basket {
     private String[] products;
     private int[] price;
+    int[] productsInCart;
 
     public Basket (String[] products, int[] price) {
         this.products = products;
         this.price = price;
+        productsInCart = new int[products.length];
+    }
+
+    public void addToCart (int productNum, int amount) {
+        productsInCart[productNum - 1] = amount;
     }
 }
