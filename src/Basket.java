21f8fb2c95d0500ec31340843d9580a3f31aef46
Andrey <kovorot0875@mail.ru>
2023-04-23 19:35:33 +0300

    printCart method added

--- src/Basket.java
+++ src/Basket.java
@@ -1,7 +1,8 @@
 public class Basket {
     private String[] products;
     private int[] price;
-    int[] productsInCart;
+    private int[] productsInCart;
+    private int cartPrice;
 
     public Basket (String[] products, int[] price) {
         this.products = products;
@@ -10,6 +11,19 @@ public class Basket {
     }
 
     public void addToCart (int productNum, int amount) {
-        productsInCart[productNum - 1] = amount;
+        productsInCart[productNum - 1] += amount;
+        cartPrice += price[productNum] * amount;
+    }
+
+    public void printCart() {
+        System.out.println("Ваша корзина:");
+
+        for (int i = 0; i < products.length; i++) {
+
+            if (productsInCart[i] != 0) {
+                int fullPrice = price[i] * productsInCart[i];
+                System.out.println(products[i] + " " + productsInCart[i] + " шт " + price[i] + " руб/шт " + fullPrice + " руб в сумме");
+                System.out.println("Итого " + cartPrice + " руб");
+            }
     }
 }
