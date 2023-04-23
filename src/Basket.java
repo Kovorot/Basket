2fc4a34ca412034b253137986d11b31330bab911
Andrey <kovorot0875@mail.ru>
2023-04-23 19:56:00 +0300

    saveTxt method added

--- src/Basket.java
+++ src/Basket.java
@@ -1,8 +1,11 @@
+import java.io.*;
+
 public class Basket {
     private String[] products;
     private int[] price;
     private int[] productsInCart;
     private int cartPrice;
+    private File basket;
 
     public Basket (String[] products, int[] price) {
         this.products = products;
@@ -25,5 +28,29 @@ public class Basket {
                 System.out.println(products[i] + " " + productsInCart[i] + " шт " + price[i] + " руб/шт " + fullPrice + " руб в сумме");
                 System.out.println("Итого " + cartPrice + " руб");
             }
+        }
+    }
+
+    public void SaveTxt (File textFile) {
+        textFile = new File("Basket.txt");
+
+        try {
+            textFile.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Файл открыт");
+        }
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(textFile))) {
+
+            for (int i = 0; i < products.length; i++) {
+                bw.write(productsInCart[i]);
+                bw.write(" ");
+            }
+
+            bw.newLine();
+            bw.write(cartPrice);
+        } catch (IOException e) {
+            System.out.println("Save file: IO exception");
+        }
     }
 }
