ca458897e6c26f6b7cba0c20d1dfea6cea9190cd
Andrey <kovorot0875@mail.ru>
2023-04-23 19:30:03 +0300

    constructor added

--- src/Basket.java
+++ src/Basket.java
@@ -1,2 +1,9 @@
 public class Basket {
+    private String[] products;
+    private int[] price;
+
+    public Basket (String[] products, int[] price) {
+        this.products = products;
+        this.price = price;
+    }
 }
