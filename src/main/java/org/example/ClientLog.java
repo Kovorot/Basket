26ca39a4ad811681740cbb5d3d2c0a30d45044a0
Andrey <kovorot0875@mail.ru>
2023-05-09 16:52:25 +0300

    methods added

--- src/main/java/org/example/ClientLog.java
+++ src/main/java/org/example/ClientLog.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.io.File;
+
 public class ClientLog {
+
+    protected void log (int productNum, int amount) {
+
+    }
+
+    protected void exportAsCSV (File textFile) {
+        
+    }
 }
